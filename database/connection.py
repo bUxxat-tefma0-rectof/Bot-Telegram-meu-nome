b341f7c35c2b21805bbdafd54fa320d42cce8d72
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-08-05 17:54:48 -0300

    Remove busy timeout from database connection
    Removed busy timeout setting from database connection.

--- database/connection.py
+++ database/connection.py
@@ -15,7 +15,6 @@ def get_db() -> sqlite3.Connection:
         _conn.row_factory = sqlite3.Row
         _conn.execute("PRAGMA journal_mode=WAL")
         _conn.execute("PRAGMA foreign_keys=ON")
-        _conn.execute("PRAGMA busy_timeout=5000")
     return _conn
 
 def init_database():
