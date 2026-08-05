8ff1628f3fc449fabce7dd414de5b5859e691159
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-08-05 17:17:08 -0300

    Refactor media album and gallery creation methods
    Refactor methods for creating media albums and galleries, updating type hints and limiting the number of media items processed. Added handling for promotional prices and improved caption formatting.

--- bot/midias.py
+++ bot/midias.py
@@ -1,13 +1,11 @@
-from telegram import InputMediaPhoto, InputMediaVideo
-from typing import List, Union
+from telegram import InputMediaPhoto, InputMediaVideofrom typing import List, Union, Dict
 
 class MidiasService:
     
     @staticmethod
-    def criar_album(fotos: List[str], caption: str = None) -> List[InputMediaPhoto]:
-        """Cria um álbum de fotos"""
+    def criar_album_fotos(fotos: List[str], caption: str = None) -> List[InputMediaPhoto]:
         media_group = []
-        for i, foto in enumerate(fotos):
+        for i, foto in enumerate(fotos[:10]):
             if i == 0 and caption:
                 media_group.append(InputMediaPhoto(media=foto, caption=caption[:1024], parse_mode='Markdown'))
             else:
@@ -15,30 +13,49 @@ class MidiasService:
         return media_group
     
     @staticmethod
-    def criar_album_misto(midias: List[dict]) -> List[Union[InputMediaPhoto, InputMediaVideo]]:
-        """Cria um álbum com fotos e vídeos"""
+    def criar_album_misto(midias: List[Dict]) -> List[Union[InputMediaPhoto, InputMediaVideo]]:
         media_group = []
-        for i, m in enumerate(midias):
+        for i, m in enumerate(midias[:10]):
             caption = m.get('caption', '')[:1024] if i == 0 else None
-            if m['tipo'] == 'foto':
+            if m.get('tipo') == 'foto':
                 media_group.append(InputMediaPhoto(media=m['url'], caption=caption, parse_mode='Markdown'))
-            elif m['tipo'] == 'video':
+            elif m.get('tipo') == 'video':
                 media_group.append(InputMediaVideo(media=m['url'], caption=caption, parse_mode='Markdown'))
         return media_group
     
     @staticmethod
-    def criar_galeria_produto(produto: dict) -> List[InputMediaPhoto]:
-        """Cria galeria para um produto"""
+    def criar_galeria_produto(produto: Dict) -> List[InputMediaPhoto]:
         fotos = []
         if produto.get('foto'):
             fotos.append(produto['foto'])
         if produto.get('galeria'):
+            import json
             try:
-                import json
                 galeria = json.loads(produto['galeria'])
-                fotos.extend(galeria)
+                if isinstance(galeria, list):
+                    fotos.extend(galeria)
             except:
                 pass
         
-        caption = f'📦 *{produto["nome"]}*\n💰 {produto["preco"]}'
-        return MidiasService.criar_album(fotos[:10], caption)
+        if not fotos:
+            return []
+        
+        preco = produto.get('preco_promocional') or produto.get('preco', 0)
+        caption = f'📦 *{produto["nome"]}*\n'
+        if produto.get('marca'):
+            caption += f'🏷 {produto["marca"]}\n'
+        caption += f'💰 R$ {preco:,.2f}'
+        
+        return MidiasService.criar_album_fotos(fotos[:10], caption)
+    
+    @staticmethod
+    def get_tipo_midia(file_id: str) -> str:
+        if file_id.startswith('AgAC'):
+            return 'foto'
+        elif file_id.startswith('BAAC'):
+            return 'video'
+        elif file_id.startswith('CQAC'):
+            return 'audio'
+        elif file_id.startswith('BQAC'):
+            return 'documento'
+        return 'desconhecido'
