2ecb4690b2e0c0515d38e311b6ae1dc612e9fa56
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-08-05 16:40:32 -0300

    Initialize security module with imports

---
+++ security/__init__.py
@@ -0,0 +1,6 @@
+from .auth import SecurityAuth
+from .jwt import JWTHandler
+from .permissions import PermissionHandler
+from .rate_limit import RateLimiter
+from .antifraude import AntiFraudeService
+from .criptografia import CryptoService
